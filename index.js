5461b4905f3d748d484117d22be19e139fe3308f
AsutoraGG <76235964+AsutoraGG@users.noreply.github.com>
2022-09-03 21:36:20 +0900

    Big Update & Fix
    ゲーム内コマンドを追加!
    run.batを追加
    
    pop = サーバーの人数を表示(人数/最大 (人数待ち)
    teampop = チーム人数を表示
    now = 現在の時刻を表示(ゲーム内ではなく)

--- index.js
+++ index.js
@@ -31,39 +31,72 @@ function Print(a, b, c, d, e) {
     }
 }
 
-if (existsSync('./config.json')) {
+function setTitle(title) {
+	process.stdout.write('\x1b]2;' + title + '\x1b\x5c');
+}
+
+if (existsSync('./config.json')) { /* ファイルがあるか */
     const config = require('./config.json');
 
     console.clear();
-    if(!config) {
+    if(!config) { /* もしファイルがあっても内容が書かれてなかったら */
         Print('ERROR', language.config_error, false);
         process.exit(0);
     }
-    Print('INFO', language.found_config, false);
+    Print('INFO', language.found_config, false); // ファイルが見つかりましたのお知らせ
 
-    const rustplus = new RustPlus(config.IP, config.PORT, config.ID, config.TOKEN);
+    const rustplus = new RustPlus(config.IP, config.PORT, config.ID, config.TOKEN); // RustPlusに登録する情報
 
     rustplus.on('connected', () => { /* サーバーに接続されたら */
         Print('INFO', language.connected_rustplus, false);
+        Print('INFO', language.default_prefix, false);
         setTimeout(() => console.clear(), 3000)
-    });
-
-    rustplus.on('disconnect', (d) => {
-        console.log(d);
+        if(config.Ingame.command === true) {
+            setTitle('Rust-TeamChat-CLI | Made by @AsutoraGG | Command = true')
+        } else {
+            setTitle('Rust-TeamChat-CLI | Made by @AsutoraGG | Command = false')
+        }
     });
 
     rustplus.on('error', (e) => { /* RustPlus.jsでエラーが起こったら */
         Print('ERROR', e, false);
     });
 
-    rustplus.on('message', msg => { /* メッセージを受信したら　*/
+    rustplus.on('message', msg => {
         if (msg.broadcast && msg.broadcast.teamMessage) {
             let message = msg.broadcast.teamMessage.message.message;
             let name = msg.broadcast.teamMessage.message.name;
+            let bot = '[BOT] : '
 
-            console.log("[" + getTime() + "][CHAT] : " + message);
+            console.log("[" + getTime() + "][CHAT] : " + "[" + name + "] : " + message); // This is team Chat log
+
+            if (config.Ingame.command === true) { /* InGame Commandが有効になっていたら */
+                let prefix = config.Ingame.prefix;
+
+                if(message === `${prefix}pop`) { //Pop Command
+                    rustplus.getInfo(info => {
+                        let teaminfo = info.response.info;
+                        if(teaminfo.queuedPlayers > 0) {
+                            rustplus.sendTeamMessage(bot + `${teaminfo.players}/${teaminfo.maxPlayers} (${teaminfo.queuedPlayers})`)
+                        } else { // No Queue
+                            rustplus.sendTeamMessage(bot + `${teaminfo.players}/${teaminfo.maxPlayers}`);
+                        }
+                    })
+                }
+
+                if(message === `${prefix}now`) { // get Current Time(Real World) command
+                    rustplus.sendTeamMessage(bot + language.current_time + getTime() + language.time);
+                }
+
+                if(message === `${prefix}teampop`) {// get Team Pop command
+                    rustplus.getTeamInfo(team => {
+                        let member = team.response.teamInfo.members;
+                        rustplus.sendTeamMessage(bot + language.current_pop + member.length + language.pop);
+                    })
+                }
+            }
         }
-    })
+    });
 
     const input = readLine.createInterface({ input: process.stdin });
     input.on('line', (msg) => {
@@ -72,7 +105,7 @@ if (existsSync('./config.json')) {
     });
 
     rustplus.connect();
-} 
+}
 else {
     console.clear();
     Print('INFO', language.notfound_config, false);
